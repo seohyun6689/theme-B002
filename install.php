080d9e6756e8d4a5ee19f511a75e5af359873686
ByungGu Choi <dingdong2310@gmail.com>
2016-08-29 12:17:01 +0900

    install.php, uninstall.php 파일 추가

---
+++ install.php
@@ -0,0 +1,5 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+# 테마 설치 후 실행하여 추가 정보 입력이 필요한 경우 리다이렉션으로 추가 설치 페이지로 이동한다.
+# 해당 페이지는 adm/theme_install.php 에서 include 한다.
+?>
---
+++ uninstall.php
@@ -0,0 +1,4 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+# 테마관리에서 uninstall 시 해당 페이지를 include 하여 실행한다.
+?>
